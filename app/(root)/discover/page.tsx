6e9bcca2e651cf0c2ec8f8dca3ada851ba767d7c
Jaydeep Baruah <jaydeep.geekcoder@gmail.com>
2024-06-06 18:18:29 +0530

    added all pages, components, auth, routes, convex and clerk

---
+++ app/(root)/discover/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DicoverPodcast = () => {
+  return (
+    <div>
+      <h1 className="text-20 font-bold text-white-1">Dicover Podcast</h1>
+    </div>
+  )
+}
+
+export default DicoverPodcast
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://many-tahr-42.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
